f997b932fe4cda2ffa0f69f96519b2447fdd9304
Sujitha R <sujitha.r@s.amity.edu>
2025-07-27 14:45:53 +0530

    C5

--- math.cpp
+++ math.cpp
@@ -4,3 +4,5 @@ using namespace std;
 void drawTop(int side) {
 	for (int i = 0; i < side; ++i) {
         cout << setw(side - i + 1) << "";
+	cout << "/";
+        cout << setw(side * 2 + i * 2) << "";
